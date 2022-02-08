0a8cebc0a80fdd664f6dba5ef134bbd6e596df90
Diego Villarroel <diegovillarroel@Diegos-Air.fibertel.com.ar>
2022-02-07 23:48:09 -0300

    Second change in app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> y guarda los cambios.
+          Editar  <code>src/App.js</code> y guarda los cambios.
         </p>
         <a
           className="App-link"
